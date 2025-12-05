7a8ba696022941a2dbbed9fe3d239ad2814e39d1
Buterez <buterl2@medtronic.com>
2025-12-05 21:30:42 +0100

    #

--- data_extraction/transform/routes.py
+++ data_extraction/transform/routes.py
@@ -1,8 +1,6 @@
 import data_extraction.config.config as config
 import pandas as pd
-from data_extraction.utils import default_logger as logger
 
-def transform_routes():
-    # CONVERT 
+def transform_routes(): 
     df = pd.read_excel(f"{config.OUTPUT_PATH}routes.xlsx")
     df.to_csv(f"{config.OUTPUT_PATH}routes.csv", index=False) 
